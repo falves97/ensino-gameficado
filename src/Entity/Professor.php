83217a2ede589e39a68696982148935dc1b29e4f
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-08-31 21:24:56 -0300

    feat: add relacionamentos e renomeada a tabela de subjects

--- src/Entity/Professor.php
+++ src/Entity/Professor.php
@@ -3,14 +3,53 @@
 namespace App\Entity;
 
 use App\Repository\ProfessorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProfessorRepository::class)]
 #[ORM\Table(name: 'professors')]
 class Professor extends User
 {
+    /**
+     * @var Collection<int, Subject>
+     */
+    #[ORM\OneToMany(targetEntity: Subject::class, mappedBy: 'professor')]
+    private Collection $subjects;
+
     public function __construct()
     {
         $this->setRoles(['ROLE_PROFESSOR']);
+        $this->subjects = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Subject>
+     */
+    public function getSubjects(): Collection
+    {
+        return $this->subjects;
+    }
+
+    public function addSubject(Subject $subject): static
+    {
+        if (!$this->subjects->contains($subject)) {
+            $this->subjects->add($subject);
+            $subject->setProfessor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSubject(Subject $subject): static
+    {
+        if ($this->subjects->removeElement($subject)) {
+            // set the owning side to null (unless already changed)
+            if ($subject->getProfessor() === $this) {
+                $subject->setProfessor(null);
+            }
+        }
+
+        return $this;
     }
 }
